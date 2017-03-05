a2e620a363f9f33736cc7041eef01db09b96a3fb
Jørgen Juel <jorgen@notater.net>
2017-03-05 15:15:22 +0100

    Styreside

--- templates/styret.blade.php
+++ templates/styret.blade.php
@@ -26,5 +26,38 @@
         @php($i++)
       @endwhile
     @endif
+    @if(have_rows('tidligere_styre'))
+    <?php $halvveis = count( get_field('repeater_field') ) / 2; $i = 0; ?>
+      <section class="seksjon">
+        <div class="seksjon__holder">
+          <h2>{{get_field('overskrift_tidligere') }}</h2>
+          <div class="trekkspill__holder">
+          <div class="trekkspill">
+          @while(have_rows('tidligere_styre'))<?php the_row(); ?>
+            <div class="trekkspill__enkelt">
+              <h3>{{get_sub_field('overskrift')}}</h3>
+              <div class="trekkspill__enkelt__innhold">
+                @if(get_sub_field('bilde'))
+                  <div class="trekkspill__enkelt__bg">
+                    {!! wp_get_attachment_image( get_sub_field('bilde'), 'large' ) !!}
+                  </div>
+                @endif
+                <div class="trekkspill__enkelt__tekst">
+                  {!! get_sub_field('tekst') !!}
+                </div>
+              </div>
+            </div>
+            @if($i > $halvveis)
+            <?php $i = 0; ?>
+            </div>
+            <div class="trekkspill trekkspill--andre">
+            @endif
+            <?php $i++; ?>
+          @endwhile
+          </div>
+          </div>
+        </div>
+      </section>
+    @endif
   @endwhile
 @endsection
