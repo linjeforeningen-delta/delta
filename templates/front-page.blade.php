88cda1b223f7a403287dfade97cb65561c5926e8
Jørgen Juel <jorgen@notater.net>
2017-05-01 16:08:28 +0200

    Fjernet bedpresser hvis ikke tilgjengelig

--- templates/front-page.blade.php
+++ templates/front-page.blade.php
@@ -4,12 +4,30 @@
   @if(have_posts()) <?php the_post(); ?>
   <div class="seksjon seksjon--bg" style="background-image: url( {{ the_post_thumbnail_url( 'full' ) }} )">
   </div>
+<?php
+    $args = ['post_type' => 'bedpress', 
+            'posts_per_page' => 3,
+            'orderby' => 'meta_value_num',
+            'meta_key' => 'tidspunkt',
+            'meta_query' => [
+              'key' => 'tidspunkt',
+              'value' => date('c'),
+              'type' => 'DATE',
+              'compare' => '>=']];
+    $bedpresser = new WP_Query($args);
+?>
+  @if($bedpresser->have_posts())
   <div class="seksjon seksjon--gronn">
     <div class="seksjon__holder">
       <h1>Bedrifts- og forskningspresentasjoner</h1>
-      @include('partials.bedriftspresentasjoner')
+      <div class="bedriftspresentasjoner">
+        @while ($bedpresser->have_posts()) <?php $bedpresser->the_post(); ?>
+          @include ('partials.content-bedpress')
+        @endwhile
+      </div>
     </div>
   </div>
+  @endif
 
   <div class="seksjon">
     <div class="seksjon__holder">
