3d567d54ac990d7ebf2443e17a8fe4eaa2c152a2
Jørgen Juel <jorgen@notater.net>
2017-09-03 17:23:51 +0200

    Oppdaterte kompendieside

--- templates/partials/product-summary.blade.php
+++ templates/partials/product-summary.blade.php
@@ -1,13 +1,20 @@
 
       <?php global $product; ?>
-
     {!! the_post_thumbnail( null, 'full' ) !!}
     <div class="infoboks">
       <div class="infoboks__fagkode">
         <span>{{ $product->get_sku() }}</span>
       </div>
       <div class="infoboks__lagerstatus">
-        <span>{!! wc_get_stock_html( $product ) !!}</span>
+        @if(has_term('kompendier', 'product_cat'))
+          @if($product->get_stock_quantity() > 0)
+          <span>{{ "På Lager" }}</span>
+          @else
+          <span>{{ "Ikke på lager" }}</span>
+          @endif
+        @else
+          <span>{!! wc_get_stock_html( $product ) !!}</span>
+        @endif
       </div>
       <div class="infoboks__pris">
         <span>{{ $product->get_price() }},-</span>
