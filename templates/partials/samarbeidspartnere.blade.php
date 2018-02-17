0db78ec030a468fcbc2f289332016fc5d04fc2c1
Jørgen Juel <jorgen.juel@hotmail.com>
2018-02-17 12:39:12 +0100

    Samarbeidspartner tillater lenker

--- templates/partials/samarbeidspartnere.blade.php
+++ templates/partials/samarbeidspartnere.blade.php
@@ -4,7 +4,13 @@
   ?>
   <div class="samarbeidspartnere">
   @while ($samarbeidspartnere->have_posts()) @php($samarbeidspartnere->the_post())
+    @if(get_field('lenke'))
+      <a href="{{get_field('lenke')}}">
+    @endif
     {!! get_the_post_thumbnail( null, 'full' ) !!}
+    @if(get_field('lenke'))
+      </a>
+    @endif
   @endwhile
 
    <img src="{{ App\asset_path('images/realfagsdagene.jpg')}}" width="1828" height="320" alt="Realfagsdagene" class="realfagsdagene">
